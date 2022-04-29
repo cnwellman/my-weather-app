bd426d3f8cd4d72a2c036297feb2faaf17107ae1
cnwellman <99782797+cnwellman@users.noreply.github.com>
2022-04-28 19:50:06 -0700

    Updated to show live weather from API

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
-let apiKey = '290e06f6f09fed27901478d3e4ec6d23';
-let apiUrl =
-  'https://api.openweathermap.org/data/2.5/weather?q=New York&appid={apiKey}';
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "26823b98ce91dbbc4d3ec30137c71dcb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
